72874b5a45462003dc864e2adb5cf81c3a70e57e
suryaongithub <suryaakshajk@outlook.com>
2025-10-20 20:35:24 +0530

    Added Comments

--- lib/GlossyCeramic/GCColorSchema.dart
+++ lib/GlossyCeramic/GCColorSchema.dart
@@ -1,20 +1,44 @@
 import 'dart:ui';
+/*
+* Decides the Color-Schema(scheme), for the widgets you want to use them with.
+* Every GlossyCeramic widgets will NEED a [GCColorSchema] Class
+* The Color schemes will be different for each apps and It is highly recommended-
+* -to only use one type of new-Material-Widgets in your app for consistency-
+* -but you CAN mix and match
+*/
 
 class GCColorSchema {
+
+  // Decides the background color
   final Color backgroundColor;
+
+  // Decides the color for the Glossy foreground elements
   final Color cardColor;
+
+  // Will be used in InkWell widgets for the splash effects
   final Color rippleColor;
+
+  // The shadow color for Glossy foreground elements
   final Color shadowColor;
+
+  // The active color for foreground elements. Highlights, if selected by user
   final Color activeColor;
+
+  // The inactive color for foreground elements. When not selected by user
   final Color inactiveColor;
-  final Color activeColorDull;
+
+  // For text Color
   final Color textColor;
+
+  // Icons which are aloof and not main(Auxiliary)
   final Color auxIconColor;
+
+
+  // Default colors, can be left but you SHOULD customize them yourself
 
   GCColorSchema({
     this.auxIconColor = const Color.fromARGB(255, 189, 189, 189),
     this.textColor = const Color.fromARGB(255, 115, 115, 115),
-    this.activeColorDull = const Color.fromARGB(116, 62, 66, 117),
     this.backgroundColor = const Color.fromARGB(239, 226, 243, 253),
     this.cardColor = const Color.fromARGB(115, 255, 255, 255),
     this.rippleColor =  const Color.fromARGB(134, 80, 68, 221),
--- lib/GlossyCeramic/GCTextField.dart
+++ lib/GlossyCeramic/GCTextField.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:new_material_widgets/GlossyCeramic/GCColorSchema.dart';
 
+
+/*
+* Widget for TextField of the GlossyCeramic type.
+* Takes in colorScheme, which is a GCColorSchema class, see [GCColorSchema]
+* There IS a obscurity toggle for passwords.
+* A prefix icon for the start of the screen
+* Keyboard Input type, The keyboard that pops up from the bottom-
+* -text keyboard by default
+*
+* */
 class GCTextField extends StatefulWidget {
   final GCColorSchema colorScheme;
   final TextEditingController textEditingController;
-  final String labelText;
+  final String hintText;
   final IconData icon;
   final TextInputType? keyboardType;
   final bool obscured;
 
+
+  // The parameters having required fields are, well ~ Required. Rest have defaults values stated.
   const GCTextField({
-    required this.labelText,
+    required this.hintText,
     required this.icon,
     this.keyboardType,
     this.obscured = false,
@@ -19,13 +31,20 @@ class GCTextField extends StatefulWidget {
     super.key,
   });
 
+
+  //Creates the state
   @override
   State<GCTextField> createState() => _GCTextFieldState();
 }
 
 class _GCTextFieldState extends State<GCTextField> {
+
+  // For the visibility toggle, off by default, See [toggleObscured]
+  // only works for obscured fields like password
+
   bool visible = false;
 
+  // Toggles the visibility and sets-state (update widget)
   void toggleObscured() {
     setState(() {
       visible = !visible;
@@ -34,60 +53,82 @@ class _GCTextFieldState extends State<GCTextField> {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: Duration(milliseconds: 500),
-      child: TextField(
-        controller: widget.textEditingController,
-        obscureText: widget.obscured && !visible,
-        decoration: InputDecoration(
-          focusColor: widget.colorScheme.activeColor,
-          hintText: widget.labelText,
-          prefixIcon: Icon(widget.icon),
-          hintFadeDuration: const Duration(milliseconds: 300),
-          hintStyle: TextStyle(
-            color: widget.colorScheme.textColor,
-            letterSpacing: 1.0,
-          ),
-          contentPadding: const EdgeInsets.all(9.0),
-          suffixIcon: widget.obscured
-              ? (visible
-                    ? Material(
-                        shape: const CircleBorder(),
-                        clipBehavior: Clip.antiAlias,
-                        color: Colors.transparent,
-                        child: InkWell(
-                          splashColor: widget.colorScheme.rippleColor,
-                          onTap: toggleObscured,
-                          child: SizedBox(
-                            height: 30,
-                            width: 30,
-                            child: Icon(
-                              Icons.visibility_off,
-                              color: widget.colorScheme.auxIconColor,
-                            ),
-                          ),
-                        ),
-                      )
-                    : Material(
-                        shape: const CircleBorder(),
-                        color: Colors.transparent,
-                        clipBehavior: Clip.antiAlias,
-                        child: InkWell(
-                          splashColor: widget.colorScheme.rippleColor,
-                          onTap: toggleObscured,
-                          child: SizedBox(
-                            height: 20,
-                            width: 20,
-                            child: Icon(
-                              Icons.visibility,
-                              color: widget.colorScheme.auxIconColor,
-                            ),
-                          ),
-                        ),
-                      ))
-              : null,
-          suffixIconColor: widget.colorScheme.inactiveColor,
+    return TextField(
+      controller: widget.textEditingController,
+
+      //both need to be - what we need. hence if the obscured text bool is false then there isn't a suffix icon
+      obscureText: widget.obscured && !visible,
+
+      decoration: InputDecoration(
+
+        // uses the active color
+        focusColor: widget.colorScheme.activeColor,
+
+        //uses YOUR hintText
+        hintText: widget.hintText,
+
+        //uses YOUR Icon
+        prefixIcon: Icon(widget.icon),
+
+        hintFadeDuration: const Duration(milliseconds: 300),
+
+        hintStyle: TextStyle(
+          //Uses YOUR text-color
+          color: widget.colorScheme.textColor,
+          letterSpacing: 1.0,
         ),
+        contentPadding: const EdgeInsets.all(9.0),
+
+        // Toggles the visibility and Suffix Icons to be Eye and Eye closed
+        // Uses material and Inkwell widgets for splash effect
+        suffixIcon: widget.obscured
+            ? (visible
+                  ? Material(
+                      shape: const CircleBorder(),
+                      clipBehavior: Clip.antiAlias,
+                      color: Colors.transparent,
+                      child: InkWell(
+
+                        //Uses YOUR ripple/splash color
+                        splashColor: widget.colorScheme.rippleColor,
+
+                        //calls the above func
+                        onTap: toggleObscured,
+
+                        // The Eye-Off icon with YOUR [auxIconColor]
+                        child: SizedBox(
+                          height: 30,
+                          width: 30,
+                          child: Icon(
+                            Icons.visibility_off,
+                            color: widget.colorScheme.auxIconColor,
+                          ),
+                        ),
+                      ),
+                    )
+                  : Material(
+                      shape: const CircleBorder(),
+                      color: Colors.transparent,
+                      clipBehavior: Clip.antiAlias,
+                      child: InkWell(
+
+                      //Uses YOUR ripple/splash color
+                      splashColor: widget.colorScheme.rippleColor,
+                        onTap: toggleObscured,
+                        child: SizedBox(
+                          height: 20,
+                          width: 20,
+                          child: Icon(
+
+                            // Eye open and uses YOUR auxIconColor
+                            Icons.visibility,
+                            color: widget.colorScheme.auxIconColor,
+                          ),
+                        ),
+                      ),
+                    ))
+            : null,
+        suffixIconColor: widget.colorScheme.inactiveColor,
       ),
     );
   }
